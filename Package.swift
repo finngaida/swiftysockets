fd866b510a984d8b623f2df2c3b2cdadc03810d0
Finn Gaida <f@fga.pw>
2019-06-25 10:41:39 +0200

    update package

--- Package.swift
+++ Package.swift
@@ -1,8 +1,9 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
   name:  "swiftysockets",
   dependencies: [
-    .Package(url:  "https://github.com/iachievedit/Tide", majorVersion: 0),
+    .package(url: "../Tide", .exact("0.1.1"))
   ]
 )
