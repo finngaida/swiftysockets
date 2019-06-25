a8fdcbe6c1eaec9e8490ea7956f01928ec6803d4
Finn Gaida <f@fga.pw>
2019-06-25 20:30:37 +0200

    update package.swift

--- Package.swift
+++ Package.swift
@@ -15,11 +15,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/finngaida/Tide", from: "0.1.1")
+//        .package(url: "../Tide", from: "0.1.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "swiftysockets"),
+            name: "swiftysockets") //), dependencies: ["Tide"])
+//        .systemLibrary(
+//            name: "Tide",
+//            pkgConfig: "tide",
+//            providers: [.brew(["finngaida/formulae/tide"])]
+//        )
     ]
 )
